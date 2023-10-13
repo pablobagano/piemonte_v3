968ce07032ed6844dbd1e4335cb90ae744fb9c2c
Pablo Diego Bagano Vilas Boas <pablobagano@Pablos-MacBook-Air-6.local>
2023-10-13 20:09:39 -0300

    CSS Reset, Styles and Queries , HTMLs and JavaScript scripts added

--- piemonte/urls.py
+++ piemonte/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from .views import * 
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('emprestimos/', emprestimos, name='emprestimos'),
+    path('consorcios/', consorcios, name='consorcios'),
+    path('produtos', produtos, name='produtos'),
+    path('localizacao/', localizacao, name='localizacao'),
+    path('contato/', contato, name='contato')
 ]
--- piemonte/views.py
+++ piemonte/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'piemonte/index.html')
+
+def emprestimos(request):
+    return render(request, 'piemonte/emprestimos.html')
+
+def consorcios(request):
+    return render(request, 'piemonte/consorcios.html')
+
+def produtos(request):
+    return render(request, 'piemonte/produtos.html')
+
+def localizacao(request):
+    return render(request, 'piemonte/localizacao.html')
+
+def contato(request):
+    return render(request, 'piemonte/contato.html')
