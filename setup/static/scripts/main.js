bd41c5fbba589acbdce27c46f19eb0cbffb4596d
Pablo Diego Bagano Vilas Boas <pablobagano@Pablos-MacBook-Air-6.local>
2023-10-18 08:59:03 -0300

    Dropdown button generated

--- setup/static/scripts/main.js
+++ setup/static/scripts/main.js
@@ -1,9 +1,21 @@
+console.log("Script loaded") 
+
+const menuBt = document.querySelector(".dropbtn"); 
+console.log("Menu button: ", menuBt)
+
+menuBt.addEventListener('click', () => {
+    console.log("Button working")
+    dropdown()
+})
+
 function dropdown() {
+    console.log("Function properly triggered")
     document.getElementById('meuMenu').classList.toggle("show"); 
 }
 
+// 
 window.onclick = function(event) { 
-    if (!event.target.matches('dropbtn')){
+    if (!event.target.matches('.dropbtn') && !document.getElementById('meuMenu').classList.contains('show')){
         var dropdowns = document.getElementsByClassName("conteudoMenu");
         var i; 
         for (i = 0; i < dropdowns.length; i++) {
